9675040cbac6d380299c09262a8472bf8fbcd25e
Abrahán Cortés <acortes@mtinter.com.mx>
2021-08-27 16:11:41 -0500

    Vistas basadas en funciones

---
+++ apps/libro/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import crearAutor, listarAutor 
+
+
+urlpatterns = [
+   path('crear_autor', crearAutor, name='crear_autor'),
+   path('listar_autores', listarAutor, name='listar_autor'),
+]
